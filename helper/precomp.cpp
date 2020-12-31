7ff65ac7147d3ca668add675a32d82e0b74d827a
Wouter van Kleunen <wouter.van@kleunen.nl>
2020-12-31 14:33:16 +0100

    Fix

--- helper/precomp.cpp
+++ helper/precomp.cpp
@@ -2,27 +2,27 @@
 
 namespace MQTT_NS {
 
-template class std::shared_ptr<callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, as::io_context::strand>>>>;
-template class std::shared_ptr<callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, null_strand>>>>;
+template class callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
+template class callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, null_strand>>>;
 
 template class server<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #if defined(MQTT_USE_WS)
-template class std::shared_ptr<callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, as::io_context::strand>>>>;
-template class std::shared_ptr<callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, null_strand>>>>;
+template class callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
+template class callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, null_strand>>>;
 
 template class server_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 #endif // defined(MQTT_USE_WS)
 
 #if defined(MQTT_USE_TLS)
-template class std::shared_ptr<callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>>;
-template class std::shared_ptr<callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>>;
+template class callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
+template class callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
 
 template class server_tls<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #if defined(MQTT_USE_WS)
-template class std::shared_ptr<callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>>;
-template class std::shared_ptr<callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>>;
+template class callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
+template class callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
 
 template class server_tls_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
--- helper/precomp.hpp
+++ helper/precomp.hpp
@@ -12,28 +12,28 @@
 
 namespace MQTT_NS {
 
-extern template std::shared_ptr<callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, as::io_context::strand>>>>;
-extern template std::shared_ptr<callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, null_strand>>>>;
+extern template callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
+extern template callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, null_strand>>>;
 
 extern template server<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #if defined(MQTT_USE_WS)
-extern template std::shared_ptr<callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, as::io_context::strand>>>>;
-extern template std::shared_ptr<callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, null_strand>>>>;
+extern template callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
+extern template callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, null_strand>>>;
 
 extern template server_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #endif // defined(MQTT_USE_WS)
 
 #if defined(MQTT_USE_TLS)
-extern template std::shared_ptr<callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>>;
-extern template std::shared_ptr<callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>>;
+extern template callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
+extern template callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
 
 extern template server_tls<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #if defined(MQTT_USE_WS)
-extern template std::shared_ptr<callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>>;
-extern template std::shared_ptr<callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>>;
+extern template callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
+extern template callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
 
 extern template server_tls_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
