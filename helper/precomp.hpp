8ca6dbc6a91fa4c094099791dfead453f9c5221e
Wouter van Kleunen <wouter.van@kleunen.nl>
2020-12-31 14:39:55 +0100

    Fix

--- helper/precomp.hpp
+++ helper/precomp.hpp
@@ -12,30 +12,30 @@
 
 namespace MQTT_NS {
 
-extern template callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
-extern template callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, null_strand>>>;
+extern template class callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
+extern template class callable_overlay<async_client<tcp_endpoint<as::ip::tcp::socket, null_strand>>>;
 
-extern template server<as::io_context::strand, std::mutex, std::lock_guard, 2>;
+extern template class server<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #if defined(MQTT_USE_WS)
-extern template callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
-extern template callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, null_strand>>>;
+extern template class callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, as::io_context::strand>>>;
+extern template class callable_overlay<async_client<ws_endpoint<as::ip::tcp::socket, null_strand>>>;
 
-extern template server_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
+extern template class server_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #endif // defined(MQTT_USE_WS)
 
 #if defined(MQTT_USE_TLS)
-extern template callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
-extern template callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
+extern template class callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
+extern template class callable_overlay<async_client<tcp_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
 
-extern template server_tls<as::io_context::strand, std::mutex, std::lock_guard, 2>;
+extern template class server_tls<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #if defined(MQTT_USE_WS)
-extern template callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
-extern template callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
+extern template class callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, as::io_context::strand>>>;
+extern template class callable_overlay<async_client<ws_endpoint<tls::stream<as::ip::tcp::socket>, null_strand>>>;
 
-extern template server_tls_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
+extern template class server_tls_ws<as::io_context::strand, std::mutex, std::lock_guard, 2>;
 
 #endif // defined(MQTT_USE_WS)
 
